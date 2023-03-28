176c03c012255097cb3d26f3166f340f76d643be
John Pazarzis <jpazarzis@gmail.com>
2023-03-27 22:03:51 -0400

    Adds cppsample.sh

---
+++ cppsample.sh
@@ -0,0 +1,25 @@
+#########################################################################
+#
+# Creates a cpp by cloning the sayhello project
+# which can be cloned from git@github.com:codingismycraft/sayhello.git.
+#
+# Assumes that the sayhello code is cloned under the ~/repos
+# directory.
+#
+# Should receive the project name as the first argument of the
+# script.
+#
+#########################################################################
+
+dirname=$1
+
+if [[ ${#dirname} -le 0  ||  ${#dirname} -gt 12 ]]; then
+    echo "invalid name (length should be less than 12)."
+    exit
+fi
+
+mkdir ${dirname}
+cd ${dirname}
+cp -R ~/repos/sayhello/* .
+make clean
+
