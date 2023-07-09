f6b90fbfa0ffbf7b682bc9b7fdc931372fca0244
John Pazarzis <jpazarzis@gmail.com>
2023-07-08 20:27:42 -0400

    Adds the pylintrc configuration file.

--- install.sh
+++ install.sh
@@ -43,4 +43,4 @@ create_soft_link $SCRIPT_DIR/gl.sh $HOME_DIR/gl.sh
 create_soft_link $SCRIPT_DIR/git-log-personal.sh $HOME_DIR/git-log-personal.sh
 create_soft_link $SCRIPT_DIR/conkyrc $HOME_DIR/.conkyrc
 create_soft_link $SCRIPT_DIR/cppsample.sh $HOME_DIR/cppsample.sh
-
+create_soft_link $SCRIPT_DIR/pylintrc $HOME_DIR/.pylintrc
