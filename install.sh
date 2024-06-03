f2f72ddd48a056429b315d1a825f669ac66aafaa
john <codingismycraft@gmail.com>
2024-06-03 13:48:35 -0400

    Makes the conkyrc creation dynamic.

--- install.sh
+++ install.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Creates the soft links for the dotfile and the any other script needed.
-
 echo -n "Continue create the links under your home directory? (y/n)"
 read -r user_selection
 
@@ -9,12 +7,18 @@ if [ "$user_selection" != "y" ]; then
     exit 1
 fi
 
-
 # Retrieve the dot file directories.
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && (pwd -W 2> /dev/null || pwd))
 
 # Specify the home directory where the dot files will copied.
 HOME_DIR=/home/$USER
+
+# Install vundle if needed..
+VUNDLE_DIR=$HOME_DIR/.vim/bundle/Vundle.vim
+if [ ! -d "$VUNDLE_DIR" ]; then
+  git clone https://github.com/VundleVim/Vundle.vim.git $VUNDLE_DIR
+fi
+
 
 function create_soft_link {
     # Creates the soft link.
@@ -26,11 +30,11 @@ function create_soft_link {
 
     if test -f "$DEST_FILE"; then
         echo "$DEST_FILE exists and will be deleted."
-        rm $DEST_FILE
+        sudo rm $DEST_FILE
     fi
 
     echo Creating symbolic link $DEST_FILE
-    ln -s  $DOT_FILE $DEST_FILE
+    sudo ln -s  $DOT_FILE $DEST_FILE
 }
 
 # Creates the autostart directory if it does not exist.
@@ -45,19 +49,31 @@ create_soft_link $SCRIPT_DIR/tmux.conf $HOME_DIR/.tmux.conf
 create_soft_link $SCRIPT_DIR/ideavimrc $HOME_DIR/.ideavimrc
 create_soft_link $SCRIPT_DIR/gl.sh $HOME_DIR/gl.sh
 create_soft_link $SCRIPT_DIR/git-log-personal.sh $HOME_DIR/git-log-personal.sh
-create_soft_link $SCRIPT_DIR/conkyrc $HOME_DIR/.conkyrc
 create_soft_link $SCRIPT_DIR/cppsample.sh $HOME_DIR/cppsample.sh
 create_soft_link $SCRIPT_DIR/pylintrc $HOME_DIR/.pylintrc
 create_soft_link $SCRIPT_DIR/conky.desktop $HOME_DIR/.config/autostart/conky.desktop
-create_soft_link $SCRIPT_DIR/scripts/remote_git_urls.py /usr/local/bin/remote_git_urls.py 
+create_soft_link $SCRIPT_DIR/scripts/remote_git_urls.py /usr/local/bin/remote_git_urls.py
 create_soft_link $SCRIPT_DIR/scripts/make_docstr.py /usr/local/bin/make_docstr.py 
 create_soft_link $SCRIPT_DIR/scripts/make_unit_test.py /usr/local/bin/make_unit_test.py
 
+# Check if nvidia-smi is installed.
+if ! command -v nvidia-smi /dev/null
+then
+NVIDIA_INSTALLED=0
+else
+NVIDIA_INSTALLED=1
+fi
+
 # Create the conkyrc file.
-$SCRIPT_DIR/scripts/create_conky_rc.py
+$SCRIPT_DIR/scripts/create_conky_rc.py $USER $NVIDIA_INSTALLED
 
 # Copy the termitor config. Firstly create the directory if needed and then
 # create the soft link.
 TERMINATOR_CONF_DIR=$HOME_DIR/.config/terminator
 mkdir -p $TERMINATOR_CONF_DIR
 create_soft_link $SCRIPT_DIR/terminator-config $TERMINATOR_CONF_DIR/config
+
+# Copy the color scheme to the vim colors directory.
+VIM_COLOR_SCHEME_DIR=$HOME_DIR/.vim/colors
+mkdir -p $VIM_COLOR_SCHEME_DIR
+cp $SCRIPT_DIR/glacier.vim $VIM_COLOR_SCHEME_DIR
--- scripts/create_conky_rc.py
+++ scripts/create_conky_rc.py
@@ -14,16 +14,28 @@ to the found CPUs.
 
 import multiprocessing
 import os
-from pathlib import Path
+import sys
 
-home_dir = str(Path.home())
+assert len(sys.argv) == 3, "You must pass the user name and a " \
+                           "flag to indicate if nvidia is installed " \
+                           "(1 or 0) in the command line."
+USER_NAME = sys.argv[1]
+assert USER_NAME.strip() != "root", "Cannot install dotfiles for root"
+home_dir = os.path.join("/", "home", USER_NAME)
+assert os.path.isdir(home_dir), "Invalid home directory: " + home_dir
+
+NVIDIA_IS_AVAILABLE = bool(int(str(sys.argv[2])))
 
 _CURRENT_DIR = os.path.dirname(os.path.realpath(__file__))
 _CONKEYRC_FILEPATH = os.path.join(home_dir, ".conkyrc")
 
 
 def make_cpu_content():
-    """Prints the cores to the screen in a conkey friendly format."""
+    """Returns the conky config for the cores.
+
+    :returns: The lines to add to the conkyrc for the CPUs.
+    :rtype: list
+    """
     count = multiprocessing.cpu_count()
     x = []
     for i in range(count):
@@ -32,18 +44,37 @@ def make_cpu_content():
     return x
 
 
+def make_gpu_content():
+    """Returns the conky config for the GPU related section.
+
+    :returns: The lines to add to the conkyrc GPU section.
+    :rtype: list
+    """
+    lines = [
+        "GPU Temp ${execi 60 nvidia-settings -query [gpu:0]/GPUCoreTemp -t}°C",
+        "${color FFA300}GPU:$color${font}${exec nvidia-smi -L | cut -c 6-30}${color FFA300} Freq:$color${nvidia gpufreq} Mhz $color${color FFA300}Temperature $color${nvidia temp} C",
+        "${color FFA300}Watt:$color${font}${exec nvidia-smi -i 0 -q -d POWER |grep 'Draw' | cut -c 45-49}${color FFA300} Used:$color${font}${exec nvidia-smi -i 0 -q -d MEMORY |grep 'Used' | cut -c 45-55 | head -1}"
+    ]
+    return lines
+
+
 def main():
-    cpu_description = make_cpu_content()
     lines = []
     fn = os.path.join(_CURRENT_DIR, "..", "conkyrc")
     with open(fn) as fin:
         for line in fin:
             line = line.strip()
-            if not line.startswith("__ADD_CPUS_HERE__"):
-                lines.append(line)
-            else:
+            if line.startswith("__ADD_CPUS_HERE__"):
+                cpu_description = make_cpu_content()
                 for line1 in cpu_description:
                     lines.append(line1)
+            elif line.startswith("__ADD_NVIDIA_INFO_HERE__"):
+                if NVIDIA_IS_AVAILABLE:
+                    gpu_description = make_gpu_content()
+                    for line1 in gpu_description:
+                        lines.append(line1)
+            else:
+                lines.append(line)
 
     with open(_CONKEYRC_FILEPATH, "w") as fout:
         for line in lines:
