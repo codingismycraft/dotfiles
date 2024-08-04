2a535cd50df8554eb8d01610d4807a0282aed0e1
John Pazarzis <jpazarzis@gmail.com>
2024-08-04 19:22:53 -0400

    Adds the proper dir for konsolerc.

--- install.sh
+++ install.sh
@@ -55,8 +55,8 @@ create_soft_link $SCRIPT_DIR/conky.desktop $HOME_DIR/.config/autostart/conky.des
 create_soft_link $SCRIPT_DIR/scripts/remote_git_urls.py /usr/local/bin/remote_git_urls.py
 create_soft_link $SCRIPT_DIR/scripts/make_docstr.py /usr/local/bin/make_docstr.py 
 create_soft_link $SCRIPT_DIR/scripts/make_unit_test.py /usr/local/bin/make_unit_test.py
-create_soft_link $SCRIPT_DIR/konsolerc $HOME_DIR/konsolerc
-create_soft_link $SCRIPT_DIR/konsolerc.kmessagebox $HOME_DIR/konsolerc.kmessagebox
+create_soft_link $SCRIPT_DIR/konsolerc $HOME_DIR/.config/konsolerc
+create_soft_link $SCRIPT_DIR/konsolerc.kmessagebox $HOME_DIR/.config/konsolerc.kmessagebox
 
 # Check if nvidia-smi is installed.
 if ! command -v nvidia-smi /dev/null
